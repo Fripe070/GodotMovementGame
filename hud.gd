717a96bd01c4993ec6cefb7c8031949ca19db306
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-05-01 20:35:48 +0200

    Add grounded flag to HUD

--- hud.gd
+++ hud.gd
@@ -13,6 +13,7 @@ func _process(delta):
     $velocityLabel.text = "%v" % player.velocity
     $VelocityLabelMag.text = "%f (xz %f)" % [player.velocity.length(), vel2d.length()]
     $VelocityLabelAngle.text = "%d°" % rad_to_deg(Vector3(vel2d.x, 0, vel2d.y).angle_to(-player.transform.basis.z))
+    $groundedLabel.text = "Grounded" if player.is_on_ground else "In air"
 
     $FPSLabel.text = "FPS: %s" % int(1/delta)
 
